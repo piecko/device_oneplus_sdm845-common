d2be312ed44f4f6a351305a9d796852057377c3d
Ali B <abittin@gmail.com>
2020-03-24 21:05:32 +0300

    sdm845-common: DeviceParts: make HBM states overlayable
    op7 uses 0 for OFF, 5 for ON state so handle it
    
    Change-Id: Id2c93a47444522fa90831acf68067cc3eb1993d8

--- DeviceParts/src/com/aicp/device/HBMModeSwitch.java
+++ DeviceParts/src/com/aicp/device/HBMModeSwitch.java
@@ -33,9 +33,13 @@ public class HBMModeSwitch implements OnPreferenceChangeListener {
     public static final String SETTINGS_KEY = DeviceSettings.KEY_SETTINGS_PREFIX + DeviceSettings.KEY_HBM_SWITCH;
 
     private Context mContext;
+    private String mHBMOffState;
+    private String mHBMOnState;
 
     public HBMModeSwitch(Context context) {
         mContext = context;
+        mHBMOffState = context.getResources().getString(R.string.hbmOFF);
+        mHBMOnState = context.getResources().getString(R.string.hbmON);
     }
 
     public static String getFile() {
@@ -57,7 +61,7 @@ public class HBMModeSwitch implements OnPreferenceChangeListener {
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         Boolean enabled = (Boolean) newValue;
         Settings.System.putInt(mContext.getContentResolver(), SETTINGS_KEY, enabled ? 1 : 0);
-        Utils.writeValue(getFile(), enabled ? "1" : "0");
+        Utils.writeValue(getFile(), enabled ? mHBMOnState : mHBMOffState);
         return true;
     }
 }
